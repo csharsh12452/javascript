0ce6a87bb1a37cd3819b0893ac545dfaa613e6af
csharsh12452 <132269721+csharsh12452@users.noreply.github.com>
2024-01-31 17:42:27 +0000

    DataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert(3+3) //we are using node.js not browser
+
+console.log(3+3) 
+console.log("harsh")
+
+let name="harsh"
+let age=23
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigInt
+//String=> ""
+//boolean=>true/false
+//null => stand alone value
+//undefined =>
+//symbol => unique
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
