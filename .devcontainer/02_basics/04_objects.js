6f698d1af8dda67386f4143f8c68d54eb72bd496
Harsh Bajpai <2021csharsh12452@poornima.edu.in>
2024-02-23 22:07:50 +0530

    Object

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,52 @@
+//const tinderUser=new Object()
+const tinderUser={}
+tinderUser.id="123abc"
+tinderUser.name="Samm"
+tinderUser.isLoggedIn=false
+//console.log(tinderUser)
+
+const regularUser={
+    email:"some@gmail.com",
+    fullName:{
+        userfullName:{
+            firstName:"Harsh",
+            lastName:"Bajpai"
+        }
+    }
+}
+
+//console.log(regularUser.fullName?.userfullName.firstName)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3={5:"e",6:"f"}
+
+//const obj4={obj1,obj2}
+//const obj4=Object.assign({},obj1,obj2,obj3)
+
+const obj4={...obj1,...obj2,...obj3}
+console.log(obj4)
+
+const users=[
+    {
+        id:1,
+        email:"h@gamil.com"
+    },
+    {
+        id:1,
+        email:"h@gamil.com"
+    },
+    {
+        id:1,
+        email:"h@gamil.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
